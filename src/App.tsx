e5b6d6143772584174dcafcefdfd23815ec871ce
mr-zlaam <zlaam.dev@gmail.com>
2024-09-04 00:45:02 +0500

    initialize the random station with tailwind css
    Initialized the project named random station using tailwind, react,
    shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "./components/mode-toggle";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <ModeToggle />
+      </div>
+      App</>
+  )
+}
+
