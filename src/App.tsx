4caf372687bbe931ae7cb5273cae41e651914061
mr-zlaam <zlaam.dev@gmail.com>
2024-09-04 13:44:26 +0500

    chore: add header/navbar to the app

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import HomePage from "./app/pages/home/homePage";
 export default function App() {
   return (
     <PageWrapper className="max-w-[1000px]">
-      <Header />
+      <div className="sticky top-0 left-0 w-full border-b mb-2">
+        <Header />
+      </div>
       <HomePage />
     </PageWrapper>)
 }
--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,8 +1,20 @@
+import DivWrapper from "@/_components/DivWrapper";
+import { ModeToggle } from "@/components/mode-toggle";
+import { IoLogoGithub } from "react-icons/io";
 export default function Header() {
   return (
-    <header className="h-[80px] w-full bg-red-500">
-      <nav className="bg-blue-500 h-full flex items-center justify-around">
+    <header className="h-[80px] w-full">
+      <nav className="h-full flex items-center justify-between bg-background backdrop-blur-md">
         <h1>zlaam</h1>
+        <div className="flex items-center ">
+          <ModeToggle />
+          <DivWrapper>
+            <a href="https://github.com/mr-zlaam/random-station" target="_blank" >
+
+              <IoLogoGithub size={30} className="cursor-pointer" />
+            </a>
+          </DivWrapper>
+        </div>
 
       </nav>
     </header>
